6d5836abc498e46a545ede97d215196d9c771d9e
andrea <andrea.tassi95@gmail.com>
2019-05-06 10:59:12 +0200

    finishing of Temporal Complexity Test.

--- src/Test/TemporalComplexityLookUpTest.java
+++ src/Test/TemporalComplexityLookUpTest.java
@@ -4,6 +4,7 @@ import chord.Item;
 import chord.Node;
 import chord.InfoNode;
 
+import java.awt.*;
 import java.rmi.RemoteException;
 import java.util.*;
 
@@ -11,18 +12,16 @@ public class TemporalComplexityLookUpTest {
 
     private static ArrayList <Node> nodesInTheNetwork = new ArrayList<>();
     private static ArrayList <Item> itemsInTheNetwork = new ArrayList<>();
-    private static HashMap <Integer, Double> points = new HashMap<>();
-    private static HashMap <Integer, Double> pointsAverage = new HashMap<>();
     private static HashMap <Integer, Integer> pointsAccess = new HashMap<>();
-    private static HashMap <Integer, Integer> pointsAccessAverage = new HashMap<>();
     private static Parameters parameters = new Parameters(0);
 
-    public static void main (String args[]) throws RemoteException, InterruptedException {
-        //N = #lookUp M = #bits
+    public static void main (String args[]) throws RemoteException{
+        //N = #lookUp; M = #bits; wait = amount of time in addition to wait for each additional bit;
         int N = 10000;
         int M = 10;
         int wait = 2;
         int bit;
+        Boolean simpleLookUpAlgorithm = true;
         for (bit = 1; bit <= M; bit++){
             System.out.println("iteration for number of bits: " + bit);
             nodesInTheNetwork.clear();
@@ -31,21 +30,17 @@ public class TemporalComplexityLookUpTest {
             node0.setId(0);
             nodesInTheNetwork.add(node0);
             //create a chord full of nodes and items
-            initialize(node0, bit, wait);
-            //get the total time for do N lookup
-            long totalTime = setOfLookUp(N);
-            double averageTime = totalTime/N;
-
-            points.put(bit,(double) totalTime);
+            initialize(node0, bit, wait, simpleLookUpAlgorithm);
+            //do N lookup in the ring.
+            setOfLookUp(N);
             pointsAccess.put(bit, parameters.getCounter());
-            //System.out.println("average time: " + averageTime);
-            //System.out.println("average access: " + parameters.getCounter());
         }
-        draw(points, pointsAccess);
+        draw(pointsAccess);
     }
 
-    private static void initialize (Node node, int bit, int wait) throws RemoteException, InterruptedException {
-        node.create(bit, false);
+    //create a full ring with all items.
+    private static void initialize (Node node, int bit, int wait, boolean simpleLookUpAlgorithm) throws RemoteException {
+        node.create(bit, simpleLookUpAlgorithm);
         int size = (int) Math.pow(2,bit);
         int i;
         for (i = 1; i < size; i++){
@@ -57,7 +52,6 @@ public class TemporalComplexityLookUpTest {
             itemsInTheNetwork.add(item);
             node1.join(node);
             node1.storeItem(item);
-            //Thread.sleep(200);
         }
         long start = System.currentTimeMillis();
         long end = start + bit*wait*1000; //  *1000 ms
@@ -68,92 +62,49 @@ public class TemporalComplexityLookUpTest {
         }
     }
 
-
-    private static long setOfLookUp(int N) throws RemoteException {
+    //do N lookUp in the ring initialized in initialize method.
+    private static void setOfLookUp(int N) throws RemoteException {
         int i;
-        long totalTime = 0;
         parameters.setCounter(0);
         for (i = 0; i < N; i++){
             Collections.shuffle(nodesInTheNetwork);
             Collections.shuffle(itemsInTheNetwork);
             Node node = nodesInTheNetwork.get(0);
             Item item = itemsInTheNetwork.get(0);
-            //prints of Node that start the lookUp and Item searched.
-            //System.out.println(node.print());
-            //System.out.println(item.toString());
-
-            long startTime = System.nanoTime();
-            // TODO: 03/05/2019 fix try catch. and println
             try {
                 node.lookUp(item.getKey());
-                //System.out.println(parameters.getCounter());
             }catch (NoSuchElementException e){
                 for (Node tmp : nodesInTheNetwork) {
                     new InfoNode(tmp);
                 }
-                throw new NoSuchElementException("Element not found");
+                throw new NoSuchElementException("Element not found. Restart the program.");
             }
-            long endTime = System.nanoTime();
-            totalTime = totalTime + endTime - startTime;
-            //System.out.println(totalTime);
         }
-        return totalTime;
     }
 
-
-    private static void draw(HashMap<Integer, Double> points, HashMap<Integer, Integer> pointsAccess) {
+    //plot the total number of visits of lookUp and the number of nodes in the ring.
+    private static void draw(HashMap<Integer, Integer> pointsAccess) {
         int i;
         StringBuilder builder = new StringBuilder();
         builder.append("===================================================\n");
-        int minNodes = 2;
-        int maxNodes = (int) Math.pow(2,points.size());
-
-        //used for draw average time points
-        /*Collection<Double> times =  points.values();
-        double maxTimes = Collections.max(times);
-        double minTimes = Collections.min(times);
-        for (i = 0; i < points.size(); i++){
-            double size =  Math.pow(2, i+3);
-            double time = points.get(i + 3);
-            System.out.println("time : " + time + " nodes : " + size + "\n");
-            builder.append("time : " + time + " nodes : " + size + "\n");
-            StdDraw.setPenRadius(0.025);
-            StdDraw.setPenColor(StdDraw.BLUE);
-            double finalTime = (time - minTimes)/(maxTimes - minTimes);
-            double x = (Math.pow(2, i+3) - minNodes)/(maxNodes - minNodes);
-            builder.append("normalized nodes : " + x + " normalized time : " + finalTime + "\n");
-            StdDraw.point(x, finalTime);
-        }*/
-        StdDraw.setPenRadius(0.025);
-        StdDraw.setPenColor(StdDraw.BLUE);
-        //StdDraw.setCanvasSize(800,800);
-
-        builder.append("===================================================\n");
+        int maxNodes = (int) Math.pow(2,pointsAccess.size());
+        StdDraw.setPenRadius(0.020);
         Collection<Integer> accessList =  pointsAccess.values();
         int maxAccess = Collections.max(accessList);
-        int minAccess = Collections.min(accessList);
-
-        //StdDraw.setXscale(0, (double) maxNodes);
-        //StdDraw.setYscale(0,(double) maxAccess);
-        StdDraw.setScale(-.05, 1.05);
-
-        //System.out.println("max access: " + maxAccess);
-        //System.out.println("min access: " + minAccess);
+        StdDraw.setXscale(-180, maxNodes + 180);
+        StdDraw.setYscale(0, maxAccess + 10000);
+        Font font = new Font("Arial", Font.BOLD, 12);
+        StdDraw.setFont(font);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.text(maxNodes/2, 1000, "NUMBER OF NODES");
+        StdDraw.text(-125, maxAccess/2 + 5000, "NUMBER OF VISITS", 90);
+        StdDraw.setPenColor(StdDraw.BLUE);
         for (i = 0; i < pointsAccess.size(); i++){
-            double size =  Math.pow(2, i+1);
+            int size = (int) Math.pow(2, i+1);
             int access = pointsAccess.get(i + 1);
-            System.out.println("access : " + access + " nodes : " + size + "\n");
             builder.append("access : " + access + " nodes : " + size + "\n");
-            double tmp1 = (double) access - minAccess;
-            System.out.println(tmp1);
-            double tmp2 = (double) maxAccess - minAccess;
-            System.out.println(tmp2);
-            double normalizedAccess = tmp1/tmp2;
-            System.out.println(normalizedAccess);
-            double x = (Math.pow(2, i+1) - minNodes)/(maxNodes - minNodes);
-            builder.append("normalized nodes : " + x + " normalized access : " + normalizedAccess + "\n");
-            StdDraw.point(x, normalizedAccess);
-            //StdDraw.point(size, (double) access);
+            StdDraw.point(size, access);
+            StdDraw.text(size,access + 2000, "( " + size + ", " + access + " )");
         }
         System.out.println(builder);
     }
