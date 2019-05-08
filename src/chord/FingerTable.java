dac7eb376254fda702ee15261e1d69f77bff12c7
andrea <andrea.tassi95@gmail.com>
2019-05-08 10:44:29 +0200

    update methods of finger table to fix overflow error.

--- src/chord/FingerTable.java
+++ src/chord/FingerTable.java
@@ -7,19 +7,19 @@ import java.util.HashMap;
 
 public class FingerTable {
     private Node owner;
-    private int size;
+    private int bit;
     private HashMap<Integer,NodeInterface> map;
 
-    public FingerTable(Node node, int size) {
+    public FingerTable(Node node, int bit) {
         this.owner = node;
-        this.size = size;
+        this.bit = bit;
         this.map = new HashMap<>();
     }
 
     //given a key retrieve the successor
     public NodeInterface getClosestPrecedingNode(int key) throws RemoteException {
         int i;
-        for (i = this.size - 1; i >= 0; i--){
+        for (i = this.bit - 1; i >= 0; i--){
             NodeInterface successor = this.map.get(getPosition(i));
             int finger = successor.getId();
             if (isBetween(finger, this.owner.getId(),key)){
@@ -30,14 +30,16 @@ public class FingerTable {
     }
 
     private boolean isBetween(int key, int initial,int end){
-        int maxNodes = (int) Math.pow(2,this.size);
+        int maxNodes = (int) Math.pow(2,this.bit);
+        long tmp1 = end;
+        long tmp2 = key;
         if (end < initial){
-            end = end + maxNodes;
+            tmp1 = end + maxNodes;
             if (key < initial){
-                key = key + maxNodes;
+                tmp2 = key + maxNodes;
             }
         }
-        return key > initial && key < end;
+        return tmp2 > initial && key < tmp1;
     }
 
     public void setSuccessor(int key, NodeInterface successor){
@@ -47,34 +49,25 @@ public class FingerTable {
     //initialize the finger table entry when a node create or join in a ring.
     public void initialize(NodeInterface node) {
         int bit;
-        for (bit = 0; bit < this.size; bit++){
+        for (bit = 0; bit < this.bit; bit++){
             this.map.put(getPosition(bit), node);
         }
     }
 
     //calculate id + 2^i
     public int getPosition(int i){
-        int m = (int) Math.pow(2,this.size);
+        int m = (int) Math.pow(2,this.bit);
         i = (int) Math.pow(2,i);
-        i = i + this.owner.getId();
-        i = i % m;
+        long tmp = (i % m) + (this.owner.getId() % m);
+        i = (int) tmp % m;
         return i;
-    }
-
-    //given the key it returns the node of that position.
-    public NodeInterface getNode(int key){
-        return this.map.get(key);
-    }
-
-    public NodeInterface getOwner(){
-        return this.owner;
     }
 
     public String print() throws RemoteException{
         StringBuilder builder = new StringBuilder();
         builder.append("==================== node " + this.owner.getId() + " ==================== \n"); //40
         int i;
-        for (i = 0; i < this.size; i++){
+        for (i = 0; i < this.bit; i++){
             int index = this.getPosition(i);
             NodeInterface node = this.map.get(index);
             builder.append("          " + index + "                  " + node.print() + "\n");
